d0c0c5bafbdd7373f63e20907d98d374caca6a90
NDAKOLAHOPE <mwishandakola@gmail.com>
2026-04-18 23:24:54 +0200

    PUSH COMPLET SUR LA BRANCHE MAIN

--- SEEK_MANAGER/Program.cs
+++ SEEK_MANAGER/Program.cs
@@ -29,7 +29,6 @@ namespace SEEK_MANAGER
                     return; // user cancelled or failed to login
                 }
             }
-
             Application.Run(new MainDashboard());
         }
     }
